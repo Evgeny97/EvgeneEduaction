484b006e75da09353b2a630203e06c0c7bbe446e
Evgeny97 <48233608+Evgeny97@users.noreply.github.com>
2019-03-27 16:47:14 +0300

    Car

--- ODD_test/Program.cs
+++ ODD_test/Program.cs
@@ -6,52 +6,112 @@ using System.Threading.Tasks;
 
 namespace ODD_Car
 {
-    class Cars
+    using System;
+
+    namespace Car
     {
-        public string mark; // марка
-        public float volume;  // объем двигателя
-        public speed // текущая скорость
-        public int luggage;   // объем багажного отделения
-        public int massa;        //текущвя нагрузка
-        public bool ignition = true;     //наличие зажигание
-    }
-    public Car(string newMark, string newVolume, int newSpeed, int newLuggage, // конструктор
-    int newMassa, double newIgnition)
-    {
-        mark = newMark;
-        volume = newVolume;
-        speed = newSpeed;
-        luggage = newLuggage;
-        massa = newMassa;
-        ignition = newIgnition;
-    }
-    Cars Volvo = new Cars("volvo", 3.2, 260, 200, 2400, true);
-    class Program
-    {
-        public static void Main(string[] args);
+        class car
         {
-        Console.Write("Введите массу автомобиля - ");
-            Volvo.massa = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Масса автомобиля - {0}тонн(ы)", Volvo.massa);
+            public class Car
+            {
+                /// <summary>
+                /// марка автомобиля
+                /// </summary>
+                public string Marka;
+                private string EngineCapacity;// объем двигателя
+                protected string Speed;//текущая скорость
+                private string LuggageSpace;// объем багажного отделения
+                private string CurrentLoad;//текущая нагрузка
+                protected bool PresenceOfIgnition; //наличие зажигание 
+
+                public Car(string marka, string engineCapacity, string speed, string luggageSpace, string currentLoad)
+                {
+
+                    Marka = marka;
+                    EngineCapacity = engineCapacity;
+                    Speed = speed;
+                    LuggageSpace = luggageSpace;
+                    CurrentLoad = currentLoad;
+                }
+
+                public void StartEngine()
+                {
+                    PresenceOfIgnition = true;
+                    Console.WriteLine("Двигатель запущен");
+                }
+                public void Start()
+                {
+                    Console.WriteLine("Машина {0} {0} Завести двигатель", Marka);
+                }
+                public void Stop()
+                {
+                    Console.WriteLine("Машина {0} {0} Заглушить двигатель", Marka);
+                }
+                public void SpeedUP()
+                {
+                    Console.WriteLine("Машина {0} {0} Ускориться", Marka);
+                }
+                public void SlowDown()
+                {
+                    Console.WriteLine("Машина {0} {0} Замедлиться", Marka);
+                }
+                static void Main(string[] args)
+                {
+                    Car car = new Car("volvo", "3.2f", "0", "200", "1170");
+                    car.Start();
+                    car.Stop();
+                    car.SpeedUP();
+                    car.SlowDown();
+                    Console.ReadLine();
+                }
+                    class trucks : car
+                {
+                    public trucks(string marka, string engineCapacity, string speed, string luggageSpace, string currentLoad) 
+                {
+                    Marka = marka;
+                    EngineCapacity = engineCapacity;
+                    Speed = speed;
+                    LuggageSpace = luggageSpace;
+                    CurrentLoad = currentLoad;
+                }
+                trucks man = new trucks("man", "2.0", "0", "2000", "3240");
+
+                }
             }
-    Console.Write("Введите марку автомобиля - ");
-            Volvo.mark = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Марка автомобиля - {0} км", Volvo.mark);
-
-            Console.Write("Введите максимальную скорость автомобиля - ");
-            Volvo.speed = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Максимальная скорость - {0} км/ч", Volvo.speed);
-
-            Console.ReadKey();
         }
-}
+    }
+}  
 
+                // объем двигателя
+                //    public int Value
+                //      {
+                //         get { return Value; }
+                //         set { if (value > 0) Value = value; }
+                //     }
+                // скорость
+                //     public int speed
+                //       {
+                //           get { return speed; }
+                //          set { if (value > 0) speed = value; }
+                //       }
+                // объем багажного отделения
+                //     public int LuggageSpace
+                //   {
+                //        get { return LuggageSpace; }
+                //        set { if (value > 0) LuggageSpace = value; }
+                //      }
+                //   }
 
-
-
-
-
-
+           //     class Cars
+         //       {
+           //         public static void Main()
+         //           {
+                        //          Car A = new Car("Volvo");
+                        //        A.Start();
+                        //
+                        // A.Stop();
+           //      A.SpeedUP();
+                        //A.SlowDown();
 
                   // марка
                   // объем двигателя
